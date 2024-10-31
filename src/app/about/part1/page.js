9094aa95fc30979c2e027989ae411f1e59cb4472
Jaimin Kharadi <learnwithjaimin@gmail.com>
2024-11-01 00:18:20 +0530

    Next.js 14 Tutorial - 6 - Nested Routes

---
+++ src/app/about/part1/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Part1() {
+  return <div>This is Part1 Page</div>;
+}
+
+export default Part1;
---
+++ src/app/about/part2/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Part2() {
+  return <div>This is Part2 Page</div>;
+}
+
+export default Part2;
