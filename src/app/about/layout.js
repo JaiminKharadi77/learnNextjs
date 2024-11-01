ae04b159164a31c9e26d92a56007745a19fcbe55
Jaimin Kharadi <learnwithjaimin@gmail.com>
2024-11-01 20:03:09 +0530

    Next.js 14 Routing - 15 - Nested Layouts

---
+++ src/app/about/layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function layout({ children }) {
+  return (
+    <>
+      {children}
+      <h2>Custom Layout Applied</h2>
+    </>
+  );
+}
+
+export default layout;
