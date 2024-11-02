5ee04ed1a6ef8986517756e627e31b1bc5e2d420
Jaimin Kharadi <learnwithjaimin@gmail.com>
2024-11-02 13:44:01 +0530

    Next.js 14 Tutorial - 17 - Routing Metadata

--- src/app/dynamic/[dynamicId]/page.js
+++ src/app/dynamic/[dynamicId]/page.js
@@ -1,5 +1,13 @@
 import React from "react";
 
+export const generateMetadata = async ({ params }) => {
+  const { dynamicId } = await params;
+
+  return {
+    title: `Dyanamic Meta Data ${dynamicId}`,
+  };
+};
+
 async function DynamicId({ params }) {
   const { dynamicId } = await params;
 
