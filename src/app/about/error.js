50e4a9f566eaf54c3ac8ac94788abb433d341f1f
Jaimin Kharadi <learnwithjaimin@gmail.com>
2024-11-08 15:51:35 +0530

    Next.js 14 Tutorial - 25 - Recovering from Errors

--- src/app/about/error.js
+++ src/app/about/error.js
@@ -1,11 +1,14 @@
 "use client";
 import React from "react";
 
-function ErrorComponent({ error }) {
+function ErrorComponent({ error, reset }) {
   return (
     <div>
       <h3>Static Error Message</h3>
       <h3>ERROR MESSAGE FROM PROPS:-{error.message}</h3>
+      <button onClick={reset} className="bg-red-300 text-black p-5">
+        Try Again
+      </button>
     </div>
   );
 }
