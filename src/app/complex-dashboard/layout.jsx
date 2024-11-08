a3fb6e01e68bdcb07b0b0f74472b5a3892cb57a3
Jaimin Kharadi <learnwithjaimin@gmail.com>
2024-11-08 18:46:59 +0530

    Next.js 14 Tutorial - 30 - Conditional Routes

--- src/app/complex-dashboard/layout.jsx
+++ src/app/complex-dashboard/layout.jsx
@@ -3,19 +3,25 @@ export default function DashboardLayout({
   users,
   revenue,
   notifications,
+  login,
 }) {
+  const isLoggedIn = !true;
   return (
     <>
-      <div>
-        <h1 className="text-xl font-semibold mb-4">{children}</h1>
-        <div className="flex">
-          <div className="flex flex-col mr-4">
-            <div className="text-gray-700 mb-2">{users}</div>
-            <div className="text-gray-700">{revenue}</div>
+      {isLoggedIn ? (
+        <div>
+          <h1 className="text-xl font-semibold mb-4">{children}</h1>
+          <div className="flex">
+            <div className="flex flex-col mr-4">
+              <div className="text-gray-700 mb-2">{users}</div>
+              <div className="text-gray-700">{revenue}</div>
+            </div>
+            <div className="flex flex-1">{notifications}</div>
           </div>
-          <div className="flex flex-1">{notifications}</div>
         </div>
-      </div>
+      ) : (
+        <div>{login}</div>
+      )}
     </>
   );
 }
